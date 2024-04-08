ecd940d2ed51b94bf5911a3198cacc72afc5f9c5
luqing <1353836146@qq.com>
2024-04-08 16:54:41 +0800

    实体类删除 get() set() toString() 方法

--- spring-boot-foodapi/src/main/java/cn/bdqn/vo/DataView.java
+++ spring-boot-foodapi/src/main/java/cn/bdqn/vo/DataView.java
@@ -11,46 +11,4 @@ public class DataView<T> {
     private String msg;
     private List<T> data;
     private Map<T,T> data1;
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public List<T> getData() {
-        return data;
-    }
-
-    public void setData(List<T> data) {
-        this.data = data;
-    }
-
-    public Map<T, T> getData1() {
-        return data1;
-    }
-
-    public void setData1(Map<T, T> data1) {
-        this.data1 = data1;
-    }
-
-    @Override
-    public String toString() {
-        return "DataView{" +
-                "code=" + code +
-                ", msg='" + msg + '\'' +
-                ", data=" + data +
-                ", data1=" + data1 +
-                '}';
-    }
 }
